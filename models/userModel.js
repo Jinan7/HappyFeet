2e31c1b4e2add1e64208792b6fdc89e7d0e924ec
Jinan Lakan <lakanj25@gmail.com>
2023-08-25 07:31:18 +0200

    Create hash password pre save

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose')
-
+const bcryptjs = require('bcryptjs')
 const userSchema = new mongoose.Schema({
     firstname:String,
     lastname:String,
     email:String,
     password:String,
-    address:String,
 })
 
+userSchema.pre('save', async function(){
+    const salt = await bcryptjs.genSalt(10)
+    const hashedPassword = await bcryptjs.hash(this.password, salt)
+    this.password = hashedPassword
+})
+userSchema.methods.comparePassword= async function (password){
+    return await bcryptjs.compare(password, this.password)
+}
 
 const Users = mongoose.model('Users', userSchema)
 
