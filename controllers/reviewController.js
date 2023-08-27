8fa93c89d1f6ec4528159744c09c7291b48ac82b
Jinan Lakan <lakanj25@gmail.com>
2023-08-27 08:03:24 +0200

    Create review controller

---
+++ controllers/reviewController.js
@@ -0,0 +1,22 @@
+
+const createOne = (req, res) => {
+    return res.send()
+}
+
+const getAll = (req, res) => {
+    return res.send()
+}
+
+const getOne = (req, res) => {
+    return res.send()
+}
+
+const updateOne = (req, res) => {
+    return res.send()
+}
+
+const deleteOne = (req, res) => {
+    return res.send()
+}
+
+module.exports = {getAll, getOne, createOne, updateOne, deleteOne}
