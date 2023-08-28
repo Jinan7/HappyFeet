05c387265e66e60fb470ab87a6203c4c144ff1a5
Jinan Lakan <lakanj25@gmail.com>
2023-08-28 08:02:37 +0200

    Create navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,38 @@
+import { FaShoppingCart, FaCartPlus} from "react-icons/fa"
+import { Link } from "react-router-dom"
+import { styled } from "styled-components"
+
 
 const Navbar = () => {
-    return (<>Navbar</>)
+    return (<Wrapper>
+        <a href="/products"><h2>happy feet</h2></a>
+        <Link><FaShoppingCart className="icon"/></Link>
+    </Wrapper>)
 }
 
+const Wrapper = styled.div`
+    height: var(--navheight);
+    background: var(--clr-yellow);
+    padding: 0.2rem 1rem;
+    display:grid;
+    grid-template-columns: 1fr auto auto;
+    align-items:center;
+    position:sticky;
+    top:0;
+    left:0;
+
+    h2{
+        font-family: 'Barriecito', cursive;
+        text-transform:uppercase;
+        color: var(--clr-dark-blue);
+    }
+
+    a{
+        text-decoration:none;
+    }
+    .icon{
+        color: var(--clr-dark-blue);
+        font-size:1.5rem;
+    }
+`
 export default Navbar
