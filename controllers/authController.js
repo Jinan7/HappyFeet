fffe0d16bc225335963ef63da84f3dabdb4c57c7
Jinan Lakan <lakanj25@gmail.com>
2023-08-28 08:03:32 +0200

    Create auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,11 +2,16 @@ const Users = require('../models/userModel')
 const { generateToken } = require('../util/generateToken')
 
 const register = async (req, res) => {
-    console.log(req.body)
     const result = await Users.create(req.body)
     const {_id} = result
     const token = generateToken(_id)
-    return res.json(token)
+    const oneDay = 1000 * 3600 * 24
+    console.log(Date.now())
+    res.cookie('token',token,{
+        httpOnly:true,
+        expires:new Date(Date.now() + oneDay),
+    })
+    return res.json()
 }
 
 const login = async (req, res) => {
@@ -18,11 +23,18 @@ const login = async (req, res) => {
     if(!isValidPassword) throw new Error("Invalid credentials")
     const {_id} = result
     const token = generateToken(_id)
-    return res.send(token)
+    const oneDay = 1000 * 3600 * 24
+    res.cookie('token',token,{
+        httpOnly:true,
+        expires:new Date(Date.now() + oneDay),
+    })
+    return res.json()
 }
 
 const logout = async (req, res) => {
-    console.log("logging out")
+    res.cookie('token', '',{
+        expires:new Date(Date.now()+5000)
+    })
     return res.send()
 }
 
