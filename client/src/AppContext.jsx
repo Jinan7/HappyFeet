d01700363145ba98baeac7b3d7bf92ebb8279cc6
Jinan Lakan <lakanj25@gmail.com>
2023-08-29 01:05:07 +0200

    Persist cart data

--- client/src/AppContext.jsx
+++ client/src/AppContext.jsx
@@ -1,16 +1,39 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { reducer } from "../util/reducer";
-import { ADD_ITEM } from "../util/actions";
+import { ADD_ITEM, DELETE_ITEM } from "../util/actions";
+
+const getLocalStorage = () => {
+    let cart = localStorage.getItem('cart')
+    if(cart){
+        return JSON.parse(cart)
+    }else{
+        return []
+    }
+}
 
 export const AppContext = createContext()
+const initialState = {
+    cart: getLocalStorage(),
+    total_items: 0,
+    total_amount: 0,
+    shipping_fee: 10,
+} 
+
+
+
 
 const AppContextProvider = ({children})=>{
-    const [cart, dispatch] = useReducer(reducer, [])
+    const [state, dispatch] = useReducer(reducer, initialState)
     const addToCart = (value) => {
         dispatch({type:ADD_ITEM, payload:value})
     }
-    console.log(cart)
-    return(<AppContext.Provider value={{cart,addToCart}}>
+    useEffect(()=>{
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+    }, [state.cart])
+    const removeFromCart = (value) => {
+        dispatch({type:DELETE_ITEM, payload:value})
+    }
+    return(<AppContext.Provider value={{state,addToCart, removeFromCart}}>
         {children}
     </AppContext.Provider>)
 }
