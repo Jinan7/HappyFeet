650b6f79a8caa67490640cd76120acbc8a783316
Jinan Lakan <lakanj25@gmail.com>
2023-08-28 08:00:02 +0200

    populate product database and delete function

--- server.js
+++ server.js
@@ -6,17 +6,30 @@ const productRouter = require('./routers/productRouters')
 const notFound = require('./middlewares/notFound')
 const errorHandler = require('./middlewares/errorHandler')
 const authRouter = require('./routers/authRouter')
-
-
+const cookieParser = require('cookie-parser')
+const auth = require('./middlewares/auth')
+const userRouter = require('./routers/userRouter')
+const fileUpload = require('express-fileupload')
+const reviewRouter = require('./routers/reviewRouter')
+const orderRouter = require('./routers/orderRouters')
+const { readFileSync } = require('fs')
+const Products = require('./models/productModel')
+const path = require('path')
 
 
 const app = express()
 dotenv.config() //load environment variables
 const port = process.env.PORT   //get port
 const mongo_url = process.env.MONGO_URI  //get mongo connection string
+app.use(express.static('./public'))
 app.use(express.json())
+app.use(cookieParser())
+app.use(fileUpload())
 app.use('/api/v1/products', productRouter)
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users',auth, userRouter)
+app.use('/api/v1/reviews', reviewRouter)
+app.use('api/v1/orders', auth, orderRouter)
 app.use('*', notFound)
 app.use(errorHandler)
 
@@ -29,6 +42,7 @@ const start = async (url, port)=>{
         app.listen(port, ()=>{
             console.log(`server is listening on ${port}`)
         })
+
     } catch (e){
         console.log(e)
     }
