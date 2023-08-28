41a9c638f39301d3ecd958cb39d9458d795feea3
Jinan Lakan <lakanj25@gmail.com>
2023-08-28 08:03:51 +0200

    modify get all products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,28 +1,45 @@
+const Products = require("../models/productModel")
+const {StatusCodes} = require('http-status-codes')
+const path  = require('path')
 
 const getAll = async (req,res)=>{
-    throw new Error("Handling the error")
-    console.log("getting all")
-    return res.send()
+    
+    const result = await Products.find({})
+
+    return res.json(result)
 }
 
 const getOne = async (req, res)=>{
-    console.log("getting one")
-    return res.send()
+    
+    const {id} = req.params
+    const result = await Products.findById({_id:id})
+    console.log(result)
+    return res.json(result)
 }
 
 const createOne = async (req,res)=>{
-    console.log("creating one")
-    return res.send()
+    const result = await Products.create(req.body)
+    return res.json(result)
 }
 
 const updateOne = async (req,res)=>{
-    console.log("updating one")
-    return res.send()
+    const {id} = req.params
+    const result = await Products.findByIdAndUpdate(id, req.body, {new:true})
+    return res.json(result)
 }
 
 const deleteOne = async (req,res)=>{
-    console.log("deleting one")
+    const {id} = req.params
+    const result = await Products.findById(id)
+    if(!result) throw new Error("not found")
+    await result.remove()
     return res.send()
 }
 
-module.exports = {getAll, getOne, createOne, updateOne, deleteOne}
+const uploadImage = async (req, res)=>{
+    const {image} = req.files
+    const imagePath = path.resolve(__dirname, '../public/uploads', `${image.name}`)
+    await image.mv(imagePath)
+    return res.status(StatusCodes.OK).json({image:`/uploads/${image.name}`})
+}
+module.exports = {getAll, getOne, createOne, updateOne, deleteOne, uploadImage}
