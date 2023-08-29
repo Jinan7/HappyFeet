f277b70b2660a71394a838bc7290cf3be098908d
Jinan Lakan <lakanj25@gmail.com>
2023-08-29 05:56:01 +0200

    v4

--- client/util/reducer.js
+++ client/util/reducer.js
@@ -4,6 +4,11 @@ import { ADD_ITEM, DELETE_ITEM, CLEAR_CART } from "./actions"
 
 export const reducer = (state, action) =>{
     if(action.type === ADD_ITEM){
+        const result = state.cart.find((item)=>{
+            return item._id === action.payload.id
+        })
+
+        if (result) return state
         return {...state,cart:[...state.cart, action.payload]}
     }
 
